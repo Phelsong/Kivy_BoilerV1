29a86d57121f5571593d6634f8280b6bf742704f
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-09-05 09:51:31 -0500

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+import kivy
+
+from kivy.app import App
+
+#EXAMPLE
+# class LoginScreen(GridLayout):
+
+#     def __init__(self, **kwargs):
+#         super(LoginScreen, self).__init__(**kwargs)
+#         self.cols = 2
+#         self.add_widget(Label(text='User Name'))
+#         self.username = TextInput(multiline=False)
+#         self.add_widget(self.username)
+#         self.add_widget(Label(text='password'))
+#         self.password = TextInput(password=True, multiline=False)
+#         self.add_widget(self.password)
+
+
+
+class Red_Moon(App):
+    pass
+
+
+if __name__ == '__main__':
+    Red_Moon().run()
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Red Moon
+
+## Stack
+
+* Kivy
+
+## Package Management
+
+* Poetry
